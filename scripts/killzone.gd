7f23692df69775ce7808a7695cfcc4c2bfd97a64
PtoDraKo13 <jumanamu2002@gmail.com>
2024-10-08 20:09:15 +0200

    Enemy and dead improvement

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -5,9 +5,12 @@ extends Area2D
 
 func _on_body_entered(body):
 	print("You died lmao")
+	Engine.time_scale = 0.7
+	body.get_node("CollisionShape2D").queue_free()
 	sfx_dead.play()
 	timer.start()
 
 
 func _on_timer_timeout():
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
