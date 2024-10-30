621950e2eaa3d284ff29c6db46fd6991c7f20ba7
PtoDraKo13 <jumanamu2002@gmail.com>
2024-10-30 13:39:47 +0100

    Menu principal

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,10 +3,5 @@ extends Control
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
-
-func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
-
-
 func _on_exit_pressed() -> void:
 	get_tree().quit()
